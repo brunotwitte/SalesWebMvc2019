54ae396d8e53820032c52713fb6ee744b2c44ca0
brunotwitte <brunotwitte@gmail.com>
2023-11-28 21:24:44 -0300

    Criacao de  MIgartion com função banco de dados

--- MVCCorret2019/Startup.cs
+++ MVCCorret2019/Startup.cs
@@ -37,8 +37,10 @@ namespace MVCCorret2019
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<MVCCorret2019Context>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MVCCorret2019Context")));
+                    options.UseMySql(Configuration.GetConnectionString("MVCCorret2019Context"), Builder =>
+                    Builder.MigrationsAssembly("MVCCorret2019")));
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
