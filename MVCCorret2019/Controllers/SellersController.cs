4b151420996bd3007b0ec97b83f7c137c2c5d7ae
brunotwitte <brunotwitte@gmail.com>
2023-12-01 11:44:53 -0300

    Delete Buttom and Back to seler

--- MVCCorret2019/Controllers/SellersController.cs
+++ MVCCorret2019/Controllers/SellersController.cs
@@ -19,6 +19,7 @@ namespace MVCCorret2019.Controllers
         {
             _sellerService = sellerService;
             _departmentService = departmentService;
+
         }
         public IActionResult Index()
         {
@@ -37,6 +38,27 @@ namespace MVCCorret2019.Controllers
 
             _sellerService.Insert(seller);
             return RedirectToAction(nameof (Index));
+        }
+        public IActionResult Delete (int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
