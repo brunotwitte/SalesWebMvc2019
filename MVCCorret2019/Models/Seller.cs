4e6149343caa9dbf7c00bd630d39ff169f157d28
brunotwitte <brunotwitte@gmail.com>
2023-12-04 20:28:57 -0300

    All Locale, number and date formattin

--- MVCCorret2019/Models/Seller.cs
+++ MVCCorret2019/Models/Seller.cs
@@ -9,18 +9,30 @@ namespace MVCCorret2019.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0}  size should be between {2} and {1} ")]
         public String name { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage ="Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public String Email { get; set; }
+
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = " Birth Date ")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0},must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public Double SalaryBase { get; set; }
+
         public Department Department { get; set; }
-        public int DepartmentId  { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
